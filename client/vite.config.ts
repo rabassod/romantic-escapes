f290e1a88d1be0bd101dd61190e69558701966e2
rabassod <rabassod@gmail.com>
2025-09-08 10:23:11 +0200

    fix: netlify config and vite base path

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // 👈 importante para que genere rutas correctas
+  build: {
+    outDir: "dist"
+  }
 })
-
-
